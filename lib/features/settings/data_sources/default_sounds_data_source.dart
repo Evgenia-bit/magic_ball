90e96673e59faec6e38cedbe840995b220ea3e1a
Evgenia-bit <ev.mo4alina@yandex.ru>
2023-08-11 08:09:42 +0300

    update project

--- lib/features/settings/data_sources/default_sounds_data_source.dart
+++ lib/features/settings/data_sources/default_sounds_data_source.dart
@@ -3,4 +3,7 @@ import 'package:surf_practice_magic_ball/features/settings/domain/entity/sound.d
 final defaultSoundsDataSource = [
   Sound(title: 'Пузырёк', fileName: 'bubble.mp3', isCustom: false),
   Sound(title: 'Свист', fileName: 'svist.mp3', isCustom: false),
+  Sound(title: 'Спокойствие', fileName: 'spokoistvie.mp3', isCustom: false),
+  Sound(title: 'Трезвучие', fileName: 'trezvuchie.mp3', isCustom: false),
+  Sound(title: 'Звоночек', fileName: 'zvonok.mp3', isCustom: false),
 ];
